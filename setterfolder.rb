46c257e822987e48d960cd4534f9a4717de56dda
Joan Wilson <48629647+JoanWilson@users.noreply.github.com>
2022-03-29 09:05:00 -0300

    Add files via upload

---
+++ setterfolder.rb
@@ -0,0 +1,33 @@
+# Documentation: https://docs.brew.sh/Formula-Cookbook
+#                https://rubydoc.brew.sh/Formula
+# PLEASE REMOVE ALL GENERATED COMMENTS BEFORE SUBMITTING YOUR PULL REQUEST!
+class Setterfolder < Formula
+  desc "A terminal program for macOS which its purpose is to organise your folders"
+  homepage ""
+  url "https://github.com/JoanWilson/Setterfolder/releases/download/v1.0.0/Setterfolder"
+  sha256 "30dc87f3a28d6d4c8bb7f1e244f321542c64aaff3b44b758aeaf3f66268915c9"
+  license ""
+
+  # depends_on "cmake" => :build
+
+  def install
+    # ENV.deparallelize  # if your formula fails when building in parallel
+    # Remove unrecognized options if warned by configure
+    # https://rubydoc.brew.sh/Formula.html#std_configure_args-instance_method
+    system "./configure", *std_configure_args, "--disable-silent-rules"
+    # system "cmake", "-S", ".", "-B", "build", *std_cmake_args
+  end
+
+  test do
+    # `test do` will create, run in and delete a temporary directory.
+    #
+    # This test will fail and we won't accept that! For Homebrew/homebrew-core
+    # this will need to be a test that verifies the functionality of the
+    # software. Run the test with `brew test Setterfolder`. Options passed
+    # to `brew install` such as `--HEAD` also need to be provided to `brew test`.
+    #
+    # The installed folder is not in the path, so use the entire path to any
+    # executables being tested: `system "#{bin}/program", "do", "something"`.
+    system "false"
+  end
+end
